84183e7b6fe3534eb3aa15c80e12f79c7b5a8466
Ata-92 <atakal92@gmail.com>
2021-05-26 14:02:14 +0300

    Check validation of the inputs

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ let k = "";
 while (true) {
   arr = prompt("Please enter ONLY integer numbers and separate them by COMMA").split(",").map((i) => i.trim());
   k = prompt("Please enter one POSITIVE integer number FROM 1 to how many numbers you entered").trim();
+
+  if (arr.every((i) => (i == parseInt(i) && !i.includes("."))) && (k > 0 && k <= arr.length && k == parseInt(k))) break;
+  else alert("You entered wrong. Try again by entering ONLY integer numbers and separating them by COMMA and also entering one POSITIVE integer number FROM 1 to how many numbers you entered");
 }
