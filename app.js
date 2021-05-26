f96d5d25f2d7dc0d5f352aabac8d377c9647a516
Ata-92 <atakal92@gmail.com>
2021-05-26 14:01:15 +0300

    Take input from the user

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 let arr = "";
 let k = "";
+
+while (true) {
+  arr = prompt("Please enter ONLY integer numbers and separate them by COMMA").split(",").map((i) => i.trim());
+  k = prompt("Please enter one POSITIVE integer number FROM 1 to how many numbers you entered").trim();
+}
